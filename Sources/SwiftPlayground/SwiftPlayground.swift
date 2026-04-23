3c2c4860eea41f820727ab0442070722feb594e3
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-04-23 03:04:40 +0000

    creating a mad-lib

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,28 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-    }
+    print("Enter a number:")
+        let number1 = readLine()
+        print("Enter a noun:")
+        let noun3 = readLine()
+        print("Enter an adjective:")
+        let adjective1 = readLine()
+    print("Enter a noun:")
+        let noun1 = readLine()
+        print("Enter a verb:")
+        let verb1 = readLine()
+        print("Enter a time value e.g hour:")
+        let timevalue1 = readLine()
+        print("Enter a verb:")
+        let verb2 = readLine()
+        print("Enter a verb:")
+        let verb3 = readLine()
+        print("Enter a noun:")
+        let noun2 = readLine()
+        
+    
+
+        let madLibString = "In the future,\(noun3) looked down on earth and observed a lot. After looking for \(number1) they noticed there was a \(adjective1) \(noun1) who loved to \(verb1) every day. They liked to run for \(timevalue1) at a time. After a good run, \(noun1) liked to \(verb2) and then \(verb3) \(noun2)."
+        print(madLibString)
+    }   
 }
