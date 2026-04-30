87dd95b1a97e87b147d601bd7f5c4f0493b8d091
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-04-30 03:06:53 +0000

    booleans work

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,5 +5,20 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
+       
+        print("Enter your age:")
+
+var userInput = readLine()!
+let age = Int(userInput)!
+
+print("Enter the password:")
+userInput = readLine()!
+
+let isOldEnough = age >= 18
+let knowsSecret = userInput == "halibut"
+let canEnterBar = isOldEnough && knowsSecret
+
+print(canEnterBar)
+
     }
 }
