93e40a03606fabc85522c6a2e88488a320748766
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-05-24 22:30:18 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,21 +4,32 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
-       
-        print("Enter your age:")
+        var age: Int? = nil
+var attempts = 0
 
-var userInput = readLine()!
-let age = Int(userInput)!
+while age == nil
+//This keeps the age 0 so the code keeps running even after you submitted an invalid response.
+ {
+    print("Enter your age:")
+    
+    if let input = readLine(), let enteredAge = Int(input) {
+        if enteredAge < 0 {
+            
+            print("Age cannot be negative. Enter a valid age.")
+            //This checks that the age isn't a negative number.
+        } else if enteredAge < 18 {
+            print("You must be 18 or older. Enter a valid age.")
+            //This checks that the age is over 18.
+        } else {
+            age = enteredAge
+        }
+        //This command says that if the code is any other integer, let the code run.
+    } else {
+        print("Invalid input. Enter a valid integer for age.")
+    }}
+//This makes sure that the number is an integer.
 
-print("Enter the password:")
-userInput = readLine()!
-
-let isOldEnough = age >= 18
-let knowsSecret = userInput == "halibut"
-let canEnterBar = isOldEnough && knowsSecret
-
-print(canEnterBar)
-
+print("Valid age entered: \(age!)")
+//This prints the age if it is valid.
     }
 }
