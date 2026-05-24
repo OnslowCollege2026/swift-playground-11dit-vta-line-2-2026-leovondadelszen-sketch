9b78588260254c1defb025508e809bf267d3dc7d
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-05-24 23:07:43 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,48 +6,47 @@ struct SwiftPlayground {
     static func main() {
         
         
-        print("Please Select an age range allowed into the bar")
-        print("Lowest Value Here!")
-    
-        var age: Int? = nil
-var attempts = 10
-var running = true
-while age == nil && running == true
-//This keeps the age 0 so the code keeps running even after you submitted an invalid response.
- {
-    print("Enter your age:")
-    
-    if let input = readLine(), let enteredAge = Int(input) {
-        if enteredAge < 0 {
-        attempts = attempts - 1 
-            print("Age cannot be negative. Enter a valid age.")
-            print("Remaining attempts = \(attempts)")
+        print("Please Select an age limit to be allowed into the bar!")
+    let agelimit = Int(readLine()!)!
+    var running = true
+
+while running == true {
+
+
+
+    if agelimit < 0 {
+    print("Age limit must be a valid age!")
+        
             //This checks that the age isn't a negative number.
-        } else if enteredAge < 18 {
-            attempts = attempts - 1 
-            print("You must be 18 or older. Enter a valid age.")
-            print("Remaining attempts = \(attempts)")
+        } else if agelimit < 16 {
+        
+            print("The limit cannot be less than 16. Enter a valid age.")
+        
             //This checks that the age is over 18.
         } else {
         running = false 
-            age = enteredAge
-            print("Valid age entered: \(age!)")
+            print("Valid age entered.") 
+            print("Only people over the age of \(agelimit) will be allowed in the Bar!")
 //This prints the age if it is valid.
         }
         //This command says that if the code is any other integer, let the code run.
-    } else {
-        attempts = attempts - 1 
+    
+    
+    
+    
+    
+    
+    
+    else {
+    
         print("Invalid input. Enter a valid integer for age.")
-        print("Remaining attempts = \(attempts)")
+        
     }
-    if attempts == 0 {
-        print("Too many incorrect attempts. You are now Locked out!")
-        running = false
-
+    
+}
     }
-    }
+}
 //This makes sure that the number is an integer.
 
 
-    }
-}
+
