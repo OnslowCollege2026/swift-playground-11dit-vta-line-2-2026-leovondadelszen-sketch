a3c387b08f3dbdc61340b38882c04243d16dcf1e
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-05-26 21:45:18 +0000

    no

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,17 +4,7 @@
 @main
 struct SwiftPlayground {
     static func main() {
-    let length: Double = 12.5
-    let width = 8.0
-    let area = length * width
-    let perimeter = 2 * (length + width)
     
-    
-        print("The length of the rectangle is \(length) units.")
-print("The width of the rectangle is \(width) units.")
-print("The area of the rectangle is \(area) square units.")
-print("The perimeter of the rectangle is \(perimeter) units.")
-
 
     
     }
