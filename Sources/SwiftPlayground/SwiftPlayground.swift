c50bb6042abe67096885ca84dc695de8cd040838
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-05-26 21:37:13 +0000

    no

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,19 +5,8 @@
 struct SwiftPlayground {
     static func main() {
         
-        let pricePerItem: Double = 19.99
-        let quantity: Double = 3.0
-        let taxRate = 15
-        let tax: Double = 0.15
-        let subtotal: Double = pricePerItem * quantity
-        let total: Double = (subtotal * tax) + subtotal
 
-print("Price per item: $\(pricePerItem)")
-print("Quantity: \(quantity)")
-print("Subtotal: $\(subtotal)")
-print("Tax (\(taxRate)%): $\(subtotal * tax)")
-print("Total: $\(total)")
-
+       
     }
 }
 
