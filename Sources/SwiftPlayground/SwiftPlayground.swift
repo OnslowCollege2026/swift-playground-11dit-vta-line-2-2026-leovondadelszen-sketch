d7bb42c173de2adf53cb623984414eff58340f6e
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-05-28 02:39:21 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,13 +4,16 @@
 @main
 struct SwiftPlayground {
     static func main() {
+
 let ticketPrice = 3.5
 let popcornPrice = 6.0
 let lolliesPrice = 8.0
-let numberOfFriends = (Int)readLine()!
-    let total = (ticketPrice * numberOfFriends) + lolliesPrice + (popcornPrice * 2)
+    
+print("How Many People are goif let numberOfFriends = readLine(), let numberOfFriends = Double(numberOfFriends)ing to the Movies?")
+ {
+let total = (ticketPrice * numberOfFriends) + lolliesPrice + (popcornPrice * 2)
 print("Total cost is: $\(total)")
-
+} else
 
     
     }
