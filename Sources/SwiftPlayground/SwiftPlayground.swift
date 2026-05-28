da63b1a31fd4434029a0384e575c2c07cf6a36c0
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-05-28 02:43:21 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -9,11 +9,12 @@ let ticketPrice = 3.5
 let popcornPrice = 6.0
 let lolliesPrice = 8.0
     
-print("How Many People are goif let numberOfFriends = readLine(), let numberOfFriends = Double(numberOfFriends)ing to the Movies?")
+print("How Many People are going to the Movies?")
+let numberOfFriends = readLine()!, let numberOfFriends = Double(numberOfFriends)
  {
 let total = (ticketPrice * numberOfFriends) + lolliesPrice + (popcornPrice * 2)
 print("Total cost is: $\(total)")
-} else
+} 
 
     
     }
