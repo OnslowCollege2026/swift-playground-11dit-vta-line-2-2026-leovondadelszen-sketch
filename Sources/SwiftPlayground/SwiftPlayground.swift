af0c56978be9f0c1e7a153d4d1df3a82c8c24a59
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-02 21:54:53 +0000

    no

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,35 +5,6 @@
 struct SwiftPlayground {
     static func main() {
 
-print("Enter the first number (left-hand side): ")
-let leftHandSide = Int(readLine()!)!
-print("Enter the second number (right-hand side): ")
-let rightHandSide = Int(readLine()!)!
-
-
-print("Type add or subtract: ")
-let calculationType = readLine()!
-
-
-func add() {
-    let answer = leftHandSide + rightHandSide
-print(answer)
-}
-
-
-func subtract() {
-    let answer = leftHandSide - rightHandSide
-    print(answer)
-}
-
-
-if calculationType == "add"{
-    add()
-} else if calculationType == "subtract"{
-    subtract()
-}
-
-
     }
 }
 
