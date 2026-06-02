5b856b601ac8c6975e831e9aa616b5028956ce89
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-02 21:37:06 +0000

    no

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,13 +5,7 @@
 struct SwiftPlayground {
     static func main() {
 
-func directionsToTimesSquare() {
-print("Walk 4 mins to 34th St Herald Square train station.")
-print("Take the Northbound N, Q, R, or W train 1 stop.")
-print("Get off the Times Square 42nd Street stop.")
-print("Take lots of pictures! 📸")
-}
-directionsToTimesSquare()
+
 
     }
 }
