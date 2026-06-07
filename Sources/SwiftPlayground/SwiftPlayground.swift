51a0db8ced5196bfee08b2158b1c603b7bc4c498
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-07 22:40:22 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,14 +5,39 @@
 struct SwiftPlayground {
     static func main() {
 var isRunning = true
+func birds() {
+print("1. Tieke")
+print("2. Kākā")
+print("3. Takahē")
+print("4. Hihi")
+print("5. Kiwi")
+print("6. Pāteke")
+print("7. Tūī")
+print("8. Kererū")
+}
+func insects() {
+print("1. Giant Wētā")
+print("2. Tree Wētā")
+print("3. Cave Wētā")
+print("4. Putoko")
+print("5. Pūngāwerewere")
+print("6. Ngaokeoke")
+print("7.  Waemano")
+print("8. Kapowai")
 
-
+}
 while isRunning == true {
 print("Welcome to Zelandia Tracker.")
     print("Did you see a Bird or Insect?")
     print("Type 'bird' for Bird or 'insect' for Insect.")
-let animalType = readLine()!
- if animal
+let animalType = readLine()!.lowercased()
+    if animalType == "bird" {
+        print("Which bird did you see?")
+print(birds)
+} else if animalType == "insect" {
+print("Which insect did you see?")
+print(insects)
+}
 
 }
 }
