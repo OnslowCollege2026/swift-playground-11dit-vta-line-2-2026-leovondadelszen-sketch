62a42bf51dba9c0f06d331b961599be30b2a7189
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-09 21:57:42 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,8 +6,8 @@ struct SwiftPlayground {
     static func main() {
 var isRunning = true
 
-let insectsSpotted: [String] = [""]
-
+var speciesSeen: [String] = []
+var numberOfSeenTime: [Int] = []
 
 while isRunning == true {
 print("Welcome to Zealandia Tracker.")
@@ -21,29 +21,56 @@ let insects: [String] = ["Giant Wētā", "Tree Wētā", "Cave Wētā", "Putoko",
 birds.enumerated().forEach{ index, bird in
 print("\(index + 1). \(bird)") }
 print("Enter a number between 1-8 to determine what bird you saw.")
-
+let birdType = Int(readLine()!)!
+let birdName = birds[birdType - 1]
+print("Added a \(birdName)")
+if speciesSeen.contains(birdName){
+    let index = speciesSeen.firstIndex(of: birdName)
+    numberOfSeenTime[index!] = numberOfSeenTime[index!] + 1
+}else {
+    speciesSeen.append(birdName)
+    numberOfSeenTime.append(1)
+}
+print("Do you want to add more Species(Y/N)")
+let confirmation = readLine()!.lowercased()
+if confirmation == "n" {
+isRunning = false
+let totalSpecies = Array(zip(speciesSeen, numberOfSeenTime))
+    totalSpecies.forEach { species in
+        print("Thank you for using Zealandia Tracker!")
+        print("Today You Saw:")
+        print("\(species.1) \(species.0)")
+}
+} else if confirmation == "y" {  
+} else {
+    isRunning = false
+    print("Invalid Input, Terminating Program")
+    let totalSpecies = Array(zip(speciesSeen, numberOfSeenTime))
+    totalSpecies.forEach { species in
+        print("Thank you for using Zealandia Tracker!")
+        print("Today You Saw:")
+        print("\(species.1) \(species.0)")
+}
+}
 } else if animalType == "insect" {
 print("Which insect did you see?")
 insects.enumerated().forEach{ index, insect in
 print("\(index + 1). \(insect)")}
 print("Enter a number between 1-8 to determine what insect you saw.")
 let insectType = Int(readLine()!)!
-switch insectType {
-case 1: print("Added a Giant Wētā")
-case 2: print("Added a Tree Wētā")
-case 3: print("Added a Cave Wētā")
-case 4: print("Added a Putoko")
-case 5: print("Added a Pūngāwerewere")
-case 6: print("Added a Ngaokeoke")
-case 7: print("Added a Waemano")
-case 8: print("Added a Kapowai")
-default: print("Incorect Value, Please Try Again.")
+let insectName = insects[insectType - 1]
+print("Added a \(insectName)")
+if speciesSeen.contains(insectName){
+    let index = speciesSeen.firstIndex(of: insectName)
+    numberOfSeenTime[index!] = numberOfSeenTime[index!] + 1
+}else {
+    speciesSeen.append(insectName)
+    numberOfSeenTime.append(1)
 }
-
 } else {
     print("Please enter a valid input. Either 'bird' or 'insect'.")
 }
-
+   
 }
 }
 }
