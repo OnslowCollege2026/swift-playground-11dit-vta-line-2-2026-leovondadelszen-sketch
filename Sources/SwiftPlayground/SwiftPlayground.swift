4e7df417f23917cea3fb7715e7b28ff33434a999
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-14 22:57:18 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,12 +6,30 @@ struct SwiftPlayground {
     static func main() {
 print("Welcome to Sleep Tracker.")
 print("Let's Start by recording your sleep.")
-print("What time did you go to sleep on Sunday night?")
-print("Please enter in 24 hour time. e.g '6' = 6am, and '18' = 6pm")
-let sundaySleepTime = readLine()!
-print("What time did you wake up on Monday Morning?")
-let mondayWakeTime = readLine()!
-let sundayNightSleep = sundaySleepTime -
+print("How many hours did you sleep on Sunday Night?")
+let sundaySleep = Int(readLine()!)!
+print("Added \(sundaySleep) hours.")
+print("How many hours did you sleep on Monday Night?")
+let mondaySleep = Int(readLine()!)!
+print("Added \(mondaySleep) hours.")
+print("How many hours did you sleep on Tuesday Night?")
+let tuesdaySleep = Int(readLine()!)!
+print("Added \(tuesdaySleep) hours.")
+print("How many hours did you sleep on Wednesday Night?")
+let wednesdaySleep = Int(readLine()!)!
+print("Added \(wednesdaySleep) hours.")
+print("How many hours did you sleep on Thursday Night?")
+let thursdaySleep = Int(readLine()!)!
+print("Added \(thursdaySleep) hours.")
+print("How many hours did you sleep on Friday Night?")
+let fridaySleep = Int(readLine()!)!
+print("Added \(fridaySleep) hours.")
+print("How many hours did you sleep on Saturday Night?")
+let saturdaySleep = Int(readLine()!)!
+print("Added \(saturdaySleep) hours.")
+let weeklySleep = (mondaySleep + tuesdaySleep + wednesdaySleep + thursdaySleep + fridaySleep + saturdaySleep + sundaySleep)
+print("You slept \(weeklySleep) hours this week!")
+print("That is a average of \(weeklySleep/7) hours a night!")
 
 }
 }
