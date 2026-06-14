0ae760f1831ceb98e65f5eac10dfc6648ff4c507
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-14 22:22:56 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,14 @@
 @main
 struct SwiftPlayground {
     static func main() {
+print("Welcome to Sleep Tracker.")
+print("Let's Start by recording your sleep.")
+print("What time did you go to sleep on Sunday night?")
+print("Please enter in 24 hour time. e.g '6' = 6am, and '18' = 6pm")
+let sundaySleepTime = readLine()!
+print("What time did you wake up on Monday Morning?")
+let mondayWakeTime = readLine()!
+let sundayNightSleep = sundaySleepTime -
 
 }
 }
