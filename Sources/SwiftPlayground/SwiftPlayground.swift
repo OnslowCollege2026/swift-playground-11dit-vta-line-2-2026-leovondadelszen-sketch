f3059cb245bb866dcf8c6bf80d9adff2a9b71a22
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-16 21:35:45 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,24 +6,29 @@ struct SwiftPlayground {
     static func main() {
 print("Welcome to Sleep Tracker.")
 print("Let's Start by recording your sleep.")
-var isInvalid = true
 let weekDays: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-while isInvalid == true{
-for weekDays.forEach { word in weekDays 
-if sundaySleep > 24 || mondaySleep > 24 || tuesdaySleep > 24 || wednesdaySleep > 24 || thursdaySleep > 24 || fridaySleep > 24 || saturdaySleep > 24 {
-print("Invalid Sleep Time. Cannot Sleep more than 24 hours in a day.")
+var totalSleep = Double(0)
+func repeating() {
+for day in weekDays {
+    print("How many hours did you sleep on \(day) night?")
+    let nightSleep = Double(readLine()!)!
+}
+repeating()
+    if nightSleep < 24 && nightSleep >= 0 {
+    totalSleep += nightSleep 
 
-}}
-let weeklySleep: Double = (mondaySleep + tuesdaySleep + wednesdaySleep + thursdaySleep + fridaySleep + saturdaySleep + sundaySleep)
-let averageSleep: Double = weeklySleep / 7.0
-print("You slept \(weeklySleep) hours this week!")
-print("That is a average of \(averageSleep) hours a night!")
+}else { print("Invalid Input please Try again!")}
+}
+
+let averageSleep = totalSleep/7
 switch averageSleep {
-case ...5 : print("Concerning Sleep Total. Seek Help")
-case 6-7: print("Okay Sleep. Could be better.")
-case 7-8: print("Decent Sleep. Good Work!")
-case 8...: print("Wow! Amazing Sleep Levels!")
-default: print("Invalid Sleep Data")
+case ...5 : print("Concerning Sleep Total. Seek Help"); print(averageSleep)
+case 6-7: print("Okay Sleep. Could be better."); print(averageSleep)
+case 7-8: print("Decent Sleep. Good Work!"); print(averageSleep)
+case 8...: print("Wow! Amazing Sleep Levels!"); print(averageSleep)
+default: print("Invalid Sleep Data"); print(averageSleep)
+
+
 }
 }
 }
