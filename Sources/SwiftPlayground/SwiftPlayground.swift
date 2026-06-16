bc59192e5b6eed4814feca388aeeaf77b039ba74
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-16 21:55:59 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,27 +8,36 @@ print("Welcome to Sleep Tracker.")
 print("Let's Start by recording your sleep.")
 let weekDays: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 var totalSleep = Double(0)
-func repeating() {
+var isRunning = true
+
+while isRunning ==  true {
 for day in weekDays {
     print("How many hours did you sleep on \(day) night?")
     let nightSleep = Double(readLine()!)!
-}
-repeating()
     if nightSleep < 24 && nightSleep >= 0 {
     totalSleep += nightSleep 
 
-}else { print("Invalid Input please Try again!")}
+}else { print("Invalid Input please Try again!")
+totalSleep = 0
 }
+    }
+
 
 let averageSleep = totalSleep/7
-switch averageSleep {
-case ...5 : print("Concerning Sleep Total. Seek Help"); print(averageSleep)
-case 6-7: print("Okay Sleep. Could be better."); print(averageSleep)
-case 7-8: print("Decent Sleep. Good Work!"); print(averageSleep)
-case 8...: print("Wow! Amazing Sleep Levels!"); print(averageSleep)
-default: print("Invalid Sleep Data"); print(averageSleep)
-
+if averageSleep >= 0 && averageSleep < 5 {
+print("Concerning Sleep Total. Seek Help"); print(averageSleep)
+} else if averageSleep >= 5 && averageSleep < 7 {
+print("Okay Sleep. Could be better."); print(averageSleep)
+} else if averageSleep >= 7 && averageSleep < 8 {
+print("Decent Sleep. Good Work!"); print(averageSleep)
+} else if averageSleep > 8 {
+print("Wow! Amazing Sleep Levels!"); print(averageSleep)
+} else {
+    print("Invalid Sleep Data. Please Try Again!"); print(averageSleep)
+}
 
 }
+
+
 }
 }
