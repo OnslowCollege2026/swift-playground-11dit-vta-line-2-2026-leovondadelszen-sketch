f5d5713cec644908b3933aa8ffd1a9350fad7610
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-21 22:49:51 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,11 +6,23 @@ struct SwiftPlayground {
     static func main() {
 
 print("Welcome to Screen Time Tracker")
-var isRunning = true
+let isRunning = true
+var totalAppTime: Double = 0.0
 let apps: [String] = ["Tiktok", "Youtube", "Instagram", "Snapchat", "Whatsapp", "Other"]
+let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 if isRunning == true {
-print(apps)
-
+days.enumerated().forEach{ index, day in
+print("On \(day) how many hours did you spend on:")
+var dayAppTime: Double = 0.0
+apps.enumerated().forEach{ index, app in
+print("\(app):")
+let appTime = Double(readLine()!)!
+dayAppTime += appTime
+}
+print(dayAppTime)
+totalAppTime += dayAppTime
+}
+print(totalAppTime)
 }
 
 }
