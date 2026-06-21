579258bf5193179534630330b674975fdaa161bf
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-21 23:13:19 +0000

    monday

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,13 +4,21 @@
 @main
 struct SwiftPlayground {
     static func main() {
-
+//Welcomes The user to my function
 print("Welcome to Screen Time Tracker")
+//Defines What 'isRunning' is and makes it true.
 let isRunning = true
+//Sets the weekly app usage to 0. Because no data has been inputted.
 var totalAppTime: Double = 0.0 
+
+//Lists the major apps that the child is using and groups them
 let apps: [String] = ["Tiktok", "Youtube", "Instagram", "Snapchat", "Whatsapp", "Other"]
+//Lists the days of the week and groups them
 let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+//This means that if the code is running then all of this code will run
 if isRunning == true {
+    print("PARENTS! Please Enter a Weekly Screen Time Limit Here:")
     let screenTimeLimit = Int(readLine()!)!
     if screenTimeLimit > 40 || screenTimeLimit < 7 {
 print("Lack of Parenting Skills.")
