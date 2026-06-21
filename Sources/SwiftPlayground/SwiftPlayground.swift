6dfc245697006404802c9aa8503c95f489b0504f
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-21 23:04:44 +0000

    yes

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,10 +7,14 @@ struct SwiftPlayground {
 
 print("Welcome to Screen Time Tracker")
 let isRunning = true
-var totalAppTime: Double = 0.0
+var totalAppTime: Double = 0.0 
 let apps: [String] = ["Tiktok", "Youtube", "Instagram", "Snapchat", "Whatsapp", "Other"]
 let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 if isRunning == true {
+    let screenTimeLimit = Int(readLine()!)!
+    if screenTimeLimit == > 40 || < 7 {
+
+    }
 days.enumerated().forEach{ index, day in
 print("On \(day) how many hours did you spend on:")
 var dayAppTime: Double = 0.0
@@ -19,10 +23,12 @@ print("\(app):")
 let appTime = Double(readLine()!)!
 dayAppTime += appTime
 }
-print(dayAppTime)
+print("Total hours spent on \(day) were: \(dayAppTime)")
 totalAppTime += dayAppTime
 }
-print(totalAppTime)
+let averageAppTime = totalAppTime / 7
+print("Total hours spent this week are: \(totalAppTime)")
+print("This is an average of \(averageAppTime) hours a day!")
 }
 
 }
