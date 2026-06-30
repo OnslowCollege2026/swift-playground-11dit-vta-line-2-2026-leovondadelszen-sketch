f36866048dff788c9aab02d52a794afff78461b3
leovondadelszen-sketch <leo.vondadelszen@student.onslow.school.nz>
2026-06-30 21:58:04 +0000

    wednesday

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -9,32 +9,34 @@ print("Welcome to Screen Time Tracker")
 
 //Defines What 'isRunning' is and makes it true.
 var isRunning = true
-//Sets the weekly app usage to 0. Because no data has been inputted.
-var totalAppTime: Double = 0.0 
 
-//Lists the major apps that the child is using and groups them
+
+//Lists the major apps that the child is using and groups them.
 let apps: [String] = ["Tiktok", "Youtube", "Instagram", "Snapchat", "Whatsapp", "Other"]
-//Lists the days of the week and groups them
+//Lists the days of the week and groups them.
 let days: [String] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-//This means that if the code is running then all of this code will run
+//This means that if the code is running then all of this code will run.
 while isRunning == true {
-   //This says that the maximum amount of screen time set can be 35 hours.
-    let maximumScreenTime = 35
-    //This says that the minimum amount of screen time set can be 7 hours.
-    let minimumScreenTime = 7
+    //Sets the weekly app usage to 0. Because no data has been inputted.
+var totalAppTime: Double = 0.0
+   //This says that the maximum amount of screen time set for the week can be 35 hours(5 Hours a day).
+    let maximumScreenTime:Double = 35
+    //This says that the minimum amount of screen time set for the week can be 7 hours(1 Hour a day).
+    let minimumScreenTime:Double = 7
     //This says that the app time is correct.
     var validAppTime = true
-    //Asks parents to input their weekly device limit for their child
-    print("PARENTS! Please Enter a Weekly Screen Time Limit Here:")
+    //Asks parents to input their daily device limit for their child for the upcoming week.
+    print("PARENTS! Please Enter a Daily Screen Time Limit for the week Here:")
 
 
-    //This checks to see if the limit the parent has set is between 7-40 weekly hours.
-if let screenTimeLimit = Int(readLine()!), screenTimeLimit >= minimumScreenTime, screenTimeLimit <= maximumScreenTime {
+    //This checks to see if the limit the parent has set is between 1-5 hours a day. If so, it will continue, and if not, it will not play any of the code and will instead skip to the last line/s of code at the very bottom of the page.
+if let screenTimeLimit = Double(readLine()!), screenTimeLimit >= minimumScreenTime/7, screenTimeLimit <= maximumScreenTime/7 {
 
 //This means that all the below code runs if the app time is correct.
 while validAppTime == true {
-
+//This sets the total app time to start at 0.
+totalAppTime = 0
 //This lets the following code play each time for every day in the week.
 for day in days { 
 // Asks how many hours the user spent on: for that specific day.
@@ -49,17 +51,18 @@ print("\(app):")
 //If the time you spent on the app is a viable time, it will add the time to your daily screen time
 if let appTime = Double(readLine()!), appTime >= 0, appTime <= 24 {
 dayAppTime += appTime
-//If the time you spent on the app isn't possible, then the code will restart.
+//If the time you spent on the app isn't possible, then the code will restart. It will aslo sen you a message letting you know that your nmber wasn't allowed.
 } else {
     print("Invalid Daily Hours")
     validAppTime = false
-    break 
+    break
     
 
 }}
-//If at any point, the amount of hours you spent on any app isn't possible, then this will break and restart the second loop of code, only triggered when the first loop o code is restarted.
+//If at any point, the amount of hours you spent on any app isn't possible, then this will break and restart the second loop of code, only triggered when the first loop of code is restarted. It will aslo sen you a message letting you know that your nmber wasn't allowed.
 if validAppTime == false || dayAppTime > 24 || dayAppTime < 0 {
     print("Invalid Daily Device Usage.")
+    print("Restarting Program. Please type correct integers in future.")
     break
 
     //If the data is possible, the computer will calculate how many hours you spent on devices that day, and add it to your weekly screen time.
@@ -76,12 +79,23 @@ let averageAppTime = totalAppTime / 7
 print("Total hours spent this week are: \(totalAppTime)")
 //Tells you your average daily screen time.
 print("This is an average of \(averageAppTime) hours a day!")
-//Stops the code from repeating
-isRunning = false 
+
+//If you succeeded in keeping below the screen time limit set by your parent, then the computer will send a message telling you that and that you did a good job. If not, they will let you know thta you didn't meet the limit and that you need to do better next time.
+if screenTimeLimit >= averageAppTime {
+print("Well Done!")
+print("You didn't exceed the daily limit!")
+} else {
+    print("You need to do Better!")
+    print("You exceeded the screen time limit set for you!")
+    
+}
+//This terminates the code, as you have got your results and there is nothing else to do.
+isRunning = false
+break
 }
 }
 } 
-//If the creen time limit set by the parent isn't reasonable. Then the question will repeat and this code will tell you that the parents answer wasn't reasonable.
+//If the screen time limit set by the parent isn't reasonable. Then the question will repeat and this code will tell you that the parents answer wasn't reasonable.
 else {
     print("Invalid Integer. Please Select a reasonable maximum weekly hours of device usage.")
 }
